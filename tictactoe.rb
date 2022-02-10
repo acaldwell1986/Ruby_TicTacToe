8a22aca00be1708eb6f3f78a0de78b8041f016b9
Andrew C <acaldwell1986@gmail.com>
2022-02-10 15:14:46 -0500

    Added player and symbol select functions

--- tictactoe.rb
+++ tictactoe.rb
@@ -15,6 +15,7 @@ def greeting
   puts 'Hello - Welcome to Ruby Tic Tac Toe!'
   sleep(1)
   puts "This game is command line only, you'll be expected to type your commands when prompted."
+  puts 'Press Any key to continue.'
   $stdin.getch
   print "            \r"
   system 'clear'
@@ -25,12 +26,13 @@ def player_selection
   puts 'Would you like to play against a human or CPU player?'
   puts 'Press 1 for Human'
   puts 'Press 2 for CPU'
-
-  # make this a loop
-  selection = gets.chomp
-  puts selection
-  puts "Here #{selection}"
+  selection = 0
+  while selection != '1' && selection != '2' do
+    selection = gets.chomp
+  end
   sleep(1)
+  system 'clear'
+  selection
 end
 
 def goodbye
@@ -41,12 +43,23 @@ def goodbye
   exit
 end
 
+def symbol_selection
+  puts 'What symbol would you like to use? (X or O)'
+  puts 'Press X or O to select.'
+  selection = ''
+  while selection.upcase != 'X' && selection.upcase != 'O' do
+    selection = gets.chomp.upcase
+  end
+  sleep(1)
+  selection
+end
+
 # method for running the game
 # commented out are the helper methods
 def game
   greeting
-  player_selection
-  # symbol_selection
+  player_type = player_selection
+  symbol_type = symbol_selection
   # generate_board
   # start_play_loop
   goodbye
